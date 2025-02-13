bd2ed0e9fe6a176801c0fff3a85bc066a86374ee
bharath <bharath@co.com>
2025-02-13 12:48:15 +0530

    created main.tf

---
+++ main.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+resource "aws_instance" "dev" {
+  ami = "ami-053b12d3152c0cc71"
+    instance_type = "t2.micro"
+    key_name = "docker"
+    tags = {
+    Name = "dev-instance"
+    env = "dev"
+}
